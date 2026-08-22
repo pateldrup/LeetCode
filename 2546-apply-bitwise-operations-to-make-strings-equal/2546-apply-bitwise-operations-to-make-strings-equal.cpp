e72be396e50324554d1aaddef490ce7f86ddd75e
Drup Patel <pateldrup.cg@gmail.com>
2026-08-22 21:42:16 +0530

    Time: 0 ms (100%), Space: 16 MB (5.85%) - LeetHub

---
+++ 2546-apply-bitwise-operations-to-make-strings-equal/2546-apply-bitwise-operations-to-make-strings-equal.cpp
@@ -0,0 +1,7 @@
+class Solution {
+public:
+    bool makeStringsEqual(string s, string target) {
+                return (s.find('1') != string::npos) == (target.find('1') != string::npos);
+
+    }
+};
