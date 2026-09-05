24d62be2f7030b31d7eee924237ca1bbaafd347f
Drup Patel <pateldrup.cg@gmail.com>
2026-09-05 22:07:36 +0530

    Time: 0 ms (100%), Space: 8.3 MB (79.42%) - LeetHub

---
+++ 0771-jewels-and-stones/0771-jewels-and-stones.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        
+       int count = 0;
+
+        for(char stone : stones) {
+
+            for(char jewel : jewels) {
+
+                if(stone == jewel) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+};
