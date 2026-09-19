17e5134117c586290fe0634f0b2e4b4febe9a77d
Drup Patel <pateldrup.cg@gmail.com>
2026-09-19 22:11:22 +0530

    Time: 0 ms (100%), Space: 8.3 MB (56.78%) - LeetHub

---
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool checkString(string s) {
+
+     bool foundB =false;
+
+     for(char ch: s){
+
+        if(ch=='b'){
+            foundB= true;
+        }
+        else if (ch=='a' && foundB){
+            return false;
+        }
+     }
+     return true;
+    }
+};
